ae2443c6cad920bd4a9de337cec47c77c9138b39
paulohenrique <paulohenrique300305@gmail.com>
2024-04-01 19:06:58 -0300

    closes #6 - JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeTrafficLight(){
+    if (document.getElementById("yellow").className === "light yellow-light") {
+        document.getElementById("red").className = "light red-light";
+        document.getElementById("yellow").className = "light light-off";
+    }
+    else{
+        if (document.getElementById("red").className === "light red-light") {
+            document.getElementById("green").className = "light green-light";
+            document.getElementById("red").className = "light light-off";
+        }
+        else{
+            document.getElementById("yellow").className = "light yellow-light";
+            document.getElementById("green").className = "light light-off";
+        }
+    }
+}
+    
+
